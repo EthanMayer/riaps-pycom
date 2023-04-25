c558a536579c9968eaf3097005ff2f3535444277
Ethan Mayer <ethan.mayer@comcast.net>
2023-04-25 08:50:58 -0500

    Closer to integration and testing

--- src/riaps/run/TEST_part.py
+++ src/riaps/run/TEST_part.py
@@ -8,12 +8,34 @@
 
 # Import everything from the project's built cython module comp
 from comp import *
+import logging
+import zmq
 
 class Part(object):
     
     def __init__(self):
+        self.logger = logging.getLogger(__name__)
         self.thread = None
+        self.name = "t"
+        self.parent = None
+        self.context = zmq.Context()
+        self.instance = None
+        self.schedulerType = None
+        self.scheduler = None
+        
+        self.control = self.context.socket(zmq.PAIR)
+        self.control.bind('inproc://part_' + self.name + '_control')
+
+    def sendControl(self, cmd, timeOut):
+        '''
+        Send a control message to component thread
+        '''
+        if self.control != None:
+            self.control.setsockopt(zmq.SNDTIMEO, timeOut) 
+            self.control.send_pyobj(cmd)
 
     def setup(self):
         self.thread = CythonComponentThread(self)     # Create component thread just like in part.py
         self.thread.start()
+        time.sleep(0.01)
+        self.sendControl("build", -1)
