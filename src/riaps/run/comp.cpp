c5a3dea49de2849aca91a3c1defb35e0ff96eb6a
Ethan Mayer <ethan.mayer@comcast.net>
2023-09-25 16:46:45 -0500

    More WIP on cppcomp header and impl

--- src/riaps/run/comp.cpp
+++ src/riaps/run/comp.cpp
@@ -12,15 +12,13 @@ class CppComponent {
     Base class for RIAPS Cython application components;
     */
 
-    CppComponent(void* object) {
+    CppComponent(std::any object) {
         /*
         Constructor
         */
-        this->GROUP_PRIORITY_MAX = 0; // Priority 0 means highest priority
-        this->GROUP_PRIORITY_MIN = 256; // Priority 256 means lowest priority (>= 256 port indices are unexpected)
-        void* class_;// = getattr('__class__');
-        std::string className;// = getattr(class_, '__name__');
-        this->owner;// = class_.OWNER; // This is set in the parent part (temporarily)
+        //std::any class_ = getattr('__class__');
+        this->className = "";// = getattr(class_, '__name__');
+        //this->owner = class_.OWNER; // This is set in the parent part (temporarily)
         //
         // Logger attributes
         // logger: logger for this class
@@ -35,7 +33,7 @@ class CppComponent {
         // this->loghandler.setFormatter(this->logformatter);
         // this->logger.addHandler(this->loghandler);
         //
-        std::string loggerName = this->owner.getActorName() + '.' + this->owner.getName();
+        this->loggerName = this->owner.getActorName() + '.' + this->owner.getName();
         this->logger = spdlog_setup.get_logger(loggerName);
         if (this->logger == NULL) {
             this->logger = spdlog.ConsoleLogger(loggerName, true, true, false);
@@ -43,225 +41,79 @@ class CppComponent {
         }
         // print  ( "Component() : '%s'" % self );
         this->coord = NULL;//Coordinator(self);
-        this->thread = NULL;
     }
  
-    void getName() {
+    std::string getName() {
         /*;
         Return the name of the component (as in model);
         */
         return this->owner.getName();
     }
     
-    void getTypeName() {
+    std::string getTypeName() {
         /*;
         Return the name of the type of the component (as in model) ;
         */
         return this->owner.getTypeName();
     }
     
-    void getLocalID() {
+    std::uint64_t getLocalID() {
         /*;
         Return a locally unique ID (int) of the component. The ID is unique within the actor.;
         */
         return id(self);
     }
 
-    void getActorName() {
+    std::string getActorName() {
         /*;
         Return the name of the parent actor (as in model);
         */
         return this->owner.getActorName();
     }
     
-    void getAppName() {
+    std::string getAppName() {
         /*;
         Return the name of the parent application (as in model);
         */
         return this->owner.getAppName();
     }
     
-    void getActorID() {
+    std::uint64_t getActorID() {
         /*;
         Return a globally unique ID (8 bytes) for the parent actor. ;
         */
         return this->owner.getActorID();
     }
     
-    void getUUID() {
+    std::uint64_t getUUID() {
         /*;
         Return the network unique ID for the parent actor;
         */
         return this->owner.getUUID();
     }
     
-    void handleActivate() {
-        /*;
-        Default activation handler;
-        */
-        ;
-    }
-    
-    void handleDeactivate() {
-        /*;
-        Default deactivation handler;
-        */
-        ;
-    }
-    
-    void handlePassivate() {
-        /*;
-        Default activation handler;
-        */
-        ;
-    }
-    
-    void handleCPULimit() {
-        /* ;
-        Default handler for CPU limit exceed
-        */
-        ;
-    }
-    
-    void handleMemLimit() {
-        /* 
-        Default handler for memory limit exceed
-        */
-        ;
-    }
-    
-    void handleSpcLimit() {
-        /* 
-        Default handler for space limit exceed
-        */
-        ;
-    }
-        
-    void handleNetLimit() {
-        /* 
-        Default handler for space limit exceed
-        */
-        ;
-    }
-    
-    void handleNICStateChange(state) {
-        /* 
-        Default handler for NIC state change
-        */
-        ;
-    }
-    
-    void handlePeerStateChange(state, uuid) {
-        /* 
-        Default handler for peer state change
-        */
-        ;
-    }
-    
-    void handleDeadline(_funcName) {
-        /*
-        Default handler for deadline violation
-        */
-        ;
-    }
-    
-    void handleGroupMessage(_group) {
-        /*
-        Default handler for group messages.
-        Implementation must immediately call recv/recv_pyobj on the group to obtain message. 
-        */
-        ;
-    }
-    
-    void handleVoteRequest(group, rfvId) {
-        /*
-        Default handler for vote requests (in member)
-        Implementation must recv/recv_pyobj to obtain the topic. 
-        */
-        ;
-    }
-    
-    void handleVoteResult(group, rfvId, vote) {
-        /*
-        Default handler for the result of a vote (in member)
-        */
-        ;
-    }
-    
-    void handleActionVoteRequest(group, rfvId, when) {
-        /*
-        Default handler for request to vote an action in the future (in member)
-        Implementation must recv/recv_pyobj to obtain the action topic. 
-        */
-        ;
-    }
-        
-    void handleMessageToLeader(group) {
-        /*
-        Default handler for messages sent to the leader (in leader)
-        Leader implementation must immediately call recv/recv_pyobj on the group to obtain message. 
-        */
-        ;
-    }
-    
-    void handleMessageFromLeader(group) {
-        /*
-        Default handler for messages received from the leader (in member) 
-        Member implementation must immediately call recv/recv_pyobj on the group to obtain message. 
-        */
-        ;
-    }
-    
-    void handleMemberJoined(group, memberId) {
-        /*
-        Default handler for 'member join' events
-        */  
-        ;
-    }
-    
-    void handleMemberLeft(group, memberId) {
-        /*
-        Default handler for 'member leave' events
-        */          
-        ;
-    }
-    
-    void handleLeaderElected(group, leaderId) {
-        /*
-        Default handler for 'leader elected' events
-        */  
-        ;
-    }
-    
-    void handleLeaderExited(void* group, std::uint64_t leaderId) {
-        /*
-        Default handler for 'leader exited' events
-        */ 
-        ;
-    }
-    
-    void* joinGroup(void* groupName, void* instName, void* groupPriority) {
-        if (groupPriority == NULL) {
+    std::any joinGroup(std::any groupName, std::any instName, std::any groupPriority) {
+        if (!groupPriority.has_value()) {
             groupPriority = this->GROUP_PRIORITY_MIN;
         }
-        if (this->thread == NULL) {
+        if (!this->thread.has_value()) {
             this->thread = this->owner.thread;
         }
-        object group = this->coord.getGroup(groupName, instName);
-        if (group == NULL) {
+        std::any group = this->coord.getGroup(groupName, instName);
+        if (!group.has_value()) {
             group = this->coord.joinGroup(this->thread, groupName, instName, this->getLocalID());
             this->thread.addGroupSocket(group, groupPriority);
         }
         return group;
     }
 
-    bool leaveGroup(void* group) {
-        assert type(group) == Group;
-        if (this->thread == NULL) {
+    bool leaveGroup(std::any group) {
+        //assert group.type().name() == Group;
+        if (!this->thread.has_value()) {
             this->thread = this->owner.thread;
         }
         this->thread.delGroupSocket(group);
         this->coord.leaveGroup(group);
-        return true
+        return true;
     }
-}
+};
--- src/riaps/run/comp.hpp
+++ src/riaps/run/comp.hpp
@@ -10,74 +10,169 @@ Created on Sept. 24, 2023
 
 #include <string>
 #include <cinttypes>
+#include <any>
 
 class CppComponent {
-    std::uint64_t GROUP_PRIORITY_MAX;
-    std::uint64_t GROUP_PRIORITY_MIN;
-    void* owner;
-    void* logger;
-    void* coord;
-    //dict __dict__ // catch-all for dynamic attributes assigned at runtime
-    void* thread;
+    // Class parameters
+    const std::uint64_t GROUP_PRIORITY_MAX = 0; // Priority 0 means highest priority
+    const std::uint64_t GROUP_PRIORITY_MIN = 256; // Priority 256 means lowest priority (>= 256 port indices are unexpected)
+    
+    std::string className = "";
+    std::string loggerName = "";
+    
+    std::any owner;
+    std::any logger;
+    std::any coord;
+    std::any thread = NULL;
 
     public:
+
+        // Base Class Functions
+        
+        /*
+        Return the name of the component (as in model);
+        */
         std::string getName();
     
+        /*
+        Return the name of the type of the component (as in model) ;
+        */
         std::string getTypeName();
         
+        /*
+        Return a locally unique ID (int) of the component. The ID is unique within the actor.;
+        */
         std::uint64_t getLocalID();
 
+        /*
+        Return the name of the parent actor (as in model);
+        */
         std::string getActorName();
         
+        /*
+        Return the name of the parent application (as in model);
+        */
         std::string getAppName();
         
+        /*
+        Return a globally unique ID (8 bytes) for the parent actor. ;
+        */
         std::uint64_t getActorID();
         
+        /*
+        Return the network unique ID for the parent actor;
+        */
         std::uint64_t getUUID();
-        
-        void handleActivate();
-        
-        void handleDeactivate();
-        
-        void handlePassivate();
-        
-        void handleCPULimit();
-        
-        void handleMemLimit();
-        
-        void handleSpcLimit();
-            
-        void handleNetLimit();
-        
-        void handleNICStateChange(void* state);
-        
-        void handlePeerStateChange(void* state, std::uint64_t uuid);
-        
-        void handleDeadline(void* _funcName);
-        
-        void handleGroupMessage(void* _group);
-        
-        void handleVoteRequest(void* group, std::uint64_t rfvId);
-        
-        void handleVoteResult(void* group, std::uint64_t rfvId, void* vote);
-        
-        void handleActionVoteRequest(void* group, std::uint64_t rfvId, void* when);
-            
-        void handleMessageToLeader(void* group);
-        
-        void handleMessageFromLeader(void* group);
-        
-        void handleMemberJoined(void* group, std::uint64_t memberId);
-        
-        void handleMemberLeft(void* group, std::uint64_t memberId);
-        
-        void handleLeaderElected(void* group, std::uint64_t leaderId);
-        
-        void handleLeaderExited(void* group, std::uint64_t leaderId);
-        
-        void* joinGroup(void* groupName, void* instName, void* groupPriority);
 
-        bool leaveGroup(void* group);
+        std::any joinGroup(std::any groupName, std::any instName, std::any groupPriority);
+
+        bool leaveGroup(std::any group);
+
+        // Pure Virtual Functions
+        
+        /*
+        Default activation handler;
+        */
+        virtual void handleActivate() = 0;
+        
+        /*
+        Default deactivation handler;
+        */
+        virtual void handleDeactivate() = 0;
+        
+        /*
+        Default activation handler;
+        */
+        virtual void handlePassivate() = 0;
+        
+        /*
+        Default handler for CPU limit exceed
+        */
+        virtual void handleCPULimit() = 0;
+        
+        /* 
+        Default handler for memory limit exceed
+        */
+        virtual void handleMemLimit() = 0;
+        
+        /* 
+        Default handler for space limit exceed
+        */
+        virtual void handleSpcLimit() = 0;
+            
+        /* 
+        Default handler for space limit exceed
+        */
+        virtual void handleNetLimit() = 0;
+        
+        /* 
+        Default handler for NIC state change
+        */
+        virtual void handleNICStateChange(std::any state) = 0;
+        
+        /* 
+        Default handler for peer state change
+        */
+        virtual void handlePeerStateChange(std::any state, std::uint64_t uuid) = 0;
+        
+        /*
+        Default handler for deadline violation
+        */
+        virtual void handleDeadline(std::any _funcName) = 0;
+        
+        /*
+        Default handler for group messages.
+        Implementation must immediately call recv/recv_pyobj on the group to obtain message. 
+        */
+        virtual void handleGroupMessage(std::any _group) = 0;
+        
+        /*
+        Default handler for vote requests (in member)
+        Implementation must recv/recv_pyobj to obtain the topic. 
+        */
+        virtual void handleVoteRequest(std::any group, std::uint64_t rfvId) = 0;
+        
+        /*
+        Default handler for the result of a vote (in member)
+        */
+        virtual void handleVoteResult(std::any group, std::uint64_t rfvId, std::any vote) = 0;
+        
+        /*
+        Default handler for the result of a vote (in member)
+        */
+        virtual void handleActionVoteRequest(std::any group, std::uint64_t rfvId, std::any when) = 0;
+            
+        /*
+        Default handler for request to vote an action in the future (in member)
+        Implementation must recv/recv_pyobj to obtain the action topic. 
+        */
+        virtual void handleMessageToLeader(std::any group) = 0;
+        
+        /*
+        Default handler for messages received from the leader (in member) 
+        Member implementation must immediately call recv/recv_pyobj on the group to obtain message. 
+        */
+        virtual void handleMessageFromLeader(std::any group) = 0;
+        
+        /*
+        Default handler for 'member join' events
+        */  
+        virtual void handleMemberJoined(std::any group, std::uint64_t memberId) = 0;
+        
+        /*
+        Default handler for 'member leave' events
+        */    
+        virtual void handleMemberLeft(std::any group, std::uint64_t memberId) = 0;
+        
+        /*
+        Default handler for 'leader elected' events
+        */  
+        virtual void handleLeaderElected(std::any group, std::uint64_t leaderId) = 0;
+        
+        /*
+        Default handler for 'leader exited' events
+        */ 
+        virtual void handleLeaderExited(std::any group, std::uint64_t leaderId) = 0;
 };
 
 #endif
